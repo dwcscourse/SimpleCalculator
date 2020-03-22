b93ce6c8f886761e9bf487ecccd0d6e3d18fe3e5
Tsvetomira Dimitrova <tsvetomira.dimitrova@docuware.com>
2020-03-22 11:16:27 +0200

    Small changes to the first version

---
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double firstNumber = double.Parse(FirstNumber.Text);
+            double secondNumber = double.Parse(SecondNumber.Text);
+
+            double result = Math.Round(firstNumber + secondNumber,2);
+
+            DisplayBox.Text = result.ToString();
+        }
+    }
+}
